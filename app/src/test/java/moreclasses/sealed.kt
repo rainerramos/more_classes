adbe8a71a35917d997aab4fd0c7294c5b0080f7b
Rainer Ramos <rainermr89@hotmail.com>
2021-08-30 12:00:17 -0300

    Sealed Class parte 2

--- app/src/test/java/moreclasses/sealed.kt
+++ app/src/test/java/moreclasses/sealed.kt
@@ -1,13 +1,35 @@
 package moreclasses
 
-open class Result {
+sealed class Result {
     class Success(val message: String) : Result()
     class Error(val message: String, val errorCode: Int) : Result()
 
-    //uma classe pode herdar a outra desde que seja OPEN
+    /*diferença da classe SEALED é: caso eu queira fazer a herança e limitar dentro do
+    mesmo arquivo que essa classe está, eu uso SEALED
+
+    É basicamente o conceite de ENUM: uma vez definido o ENUM e quais são os valores
+    dentro dele, não tem mais como alterar
+     */
+}
+
+class Repo {
+    fun execute() : Result {
+        return Result.Success("Deu certo!")
+       // return Result.Error("Deu erro!", 404)
+    }
 }
 
 fun main() {
 
+    val r1 = Repo()
+    val result: Result = r1.execute()
+    when (result) {
+        is Result.Success ->{
+            println("Deu sucesso")
+        }
+        is Result.Error -> {
+            println("Deu erro!")
+        }
+    }
 
 }
