7a5aae50c160958905dea40195d1ec1245a53df1
Rainer Ramos <rainermr89@hotmail.com>
2021-08-30 07:54:05 -0300

    inicio

---
+++ app/src/main/java/com/rainerramos/moreclasses/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rainerramos.moreclasses
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
