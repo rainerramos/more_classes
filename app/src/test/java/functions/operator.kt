56f642e869afaa698b010f6852fb55fd3973d158
Rainer Ramos <rainermr89@hotmail.com>
2021-08-31 11:40:30 -0300

    operator Overloading - parte 2

--- app/src/test/java/functions/operator.kt
+++ app/src/test/java/functions/operator.kt
@@ -9,17 +9,23 @@ data class Fraction (val numerator: Int, val denominator: Int) {
             return Fraction(((commum / denominator) * numerator) + ((commum / add.denominator) * add. numerator), commum)
         }
     }
+
+    operator fun inc(): Fraction{
+        return this
+    }
+
 }
 
 fun main(args: Array<String>){
 
-    val f1: Fraction = Fraction (3, 2)
+    var f1: Fraction = Fraction (3, 2)
     val f2: Fraction = Fraction (5, 3)
 
     println(f1 + f2)
+    f1++
 
     /*
-    utilizando o "operator", eu consigo tratar uma soma entre 2 instâncias de uma classe
+    aqui eu consigo usar o f1++ numa instância de uma classe
      */
 
 }
