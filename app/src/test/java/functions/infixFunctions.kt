80a24643893952a978529d1db887b04e2854f1e1
Rainer Ramos <rainermr89@hotmail.com>
2021-08-31 11:14:17 -0300

    Funções infix - parte 3

--- app/src/test/java/functions/infixFunctions.kt
+++ app/src/test/java/functions/infixFunctions.kt
@@ -1,5 +1,11 @@
 package functions
 
+class Person(val name: String){
+   infix fun isName(value: String): Boolean{
+        return value == name
+    }
+}
+
 infix fun Int.isHalfOf(value: Int) = value / 2 == this
 
 fun main(args: Array<String>){
@@ -7,7 +13,11 @@ fun main(args: Array<String>){
     10.isHalfOf(40)
     10 isHalfOf 30
 
-    //OBS: infix só pode ser usada quando a função recebe 1 parâmetro apenas
-    //a função infix ajuda na leitura do código, por ser um pouco mais simplificada
+    val p1: Person = Person("Rainer")
+    p1 isName "Rainer"
+
+
+    // infix também funciona para funções dentro de classes, como demonstrado acima.
+
 
 }
