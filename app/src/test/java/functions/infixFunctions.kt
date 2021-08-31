a83b25e3d07828a98bc68dbc1e0caa17dde8c648
Rainer Ramos <rainermr89@hotmail.com>
2021-08-31 11:10:00 -0300

    Funções infix - parte 2

--- app/src/test/java/functions/infixFunctions.kt
+++ app/src/test/java/functions/infixFunctions.kt
@@ -1,10 +1,13 @@
 package functions
 
-fun Int.isHalfOf(value: Int) = value / 2 == this
+infix fun Int.isHalfOf(value: Int) = value / 2 == this
 
 fun main(args: Array<String>){
 
     10.isHalfOf(40)
-    10 isHalfOf 30 // aqui temos um erro, pois não usei a palavra "infix" antes da função
+    10 isHalfOf 30
+
+    //OBS: infix só pode ser usada quando a função recebe 1 parâmetro apenas
+    //a função infix ajuda na leitura do código, por ser um pouco mais simplificada
 
 }
